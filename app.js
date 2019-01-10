b826ba06d3a8577a4562dbea6bf9a4afd5d4727b
xaviguasch <xaviguasch@gmail.com>
2019-01-10 10:49:39 +0100

    configuring passport

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const bodyParser = require('body-parser')
+const LocalStrategy = require('passport-local')
+const passportLocalMongoose = require('passport-local-mongoose')
+const User = require('./models/user')
 mongoose.connect('mongodb://localhost:27017/auth_demo_app', { useNewUrlParser: true }) 
 
 const app = express()
 
 app.set('view engine', 'ejs')
+
+app.use(require('express-session')({
+    secret: 'Xiti rules',
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 
 app.get('/', function(req, res){
     res.render('home')
